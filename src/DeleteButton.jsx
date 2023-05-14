f2623b2b1f1ea79a9e71a6162158cf2555dedeab
Adipp <emangadayanglain@gmail.com>
2023-05-14 17:46:19 +0700

    Delete text 2

--- src/DeleteButton.jsx
+++ src/DeleteButton.jsx
@@ -5,7 +5,6 @@ function Delete ({id, onDelete}) {
     return (
         <div>
             <button onClick={() => onDelete(id)} className='deletebutton'> Delete </button>
-            <box-icon type='solid' name='trash-alt' className='box-icon'></box-icon>
         </div>
     )
 }
